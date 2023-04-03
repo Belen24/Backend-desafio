b01dacfc5d44442c1419473be88298ca5c000114
Belen <bgutierrez.mil@gmail.com>
2023-04-03 12:02:32 -0400

    correcionesCodigo

--- desafio.js
+++ desafio.js
@@ -22,8 +22,14 @@ class ProductManager {
             return console.log("Codigo ingresado ya se encuentra registrado")
         }
         
-        let newId= this.products.length +1;
-        let agregarProducto = {
+        let newId;
+        if(!this.products.length){
+            newId=1;
+        }
+        else{
+            newId = this.products [this.products.length-1].id+1;
+        }
+        let newProduct = {
             id:newId,
             title,
             description,
@@ -35,8 +41,8 @@ class ProductManager {
             
         }
             
-        this.products.push(agregarProducto);
-        //console.log("Producto agredado", agregarProducto);  
+        this.products.push(newProduct);
+        
 
 
     }
@@ -45,12 +51,12 @@ class ProductManager {
     getProductById (idProducto){
      
         //Metodo para buscar ID del producto
-        let buscarIdProducto = this.products.find (producto => producto.id === idProducto);
+        let IdProduct = this.products.find (producto => producto.id === idProducto);
 
        
 
-        if(buscarIdProducto){
-            console.log(buscarIdProducto);
+        if(IdProduct){
+            console.log(IdProduct);
         }
 
         else{
@@ -62,20 +68,21 @@ class ProductManager {
 }
 
 //Proceso para agregar y visualizar productos
-let nuevoProducto= new ProductManager();
-nuevoProducto.addProduct ("Aros", "Aros luna plata 925", 5200, "https://joyaspremium.cl/wp-content/uploads/2021/12/DAD-25.png", "abc123", 25);
-nuevoProducto.addProduct ("Anillo", "Anillo de plata 925", 8500, "https://e7.pngegg.com/pngimages/798/638/png-clipart-wedding-ring-jewellery-silver-gold-ring-love-gemstone.png", "xyz456", 15);
-console.log("Productos: ", nuevoProducto.getProduct());
+let productCreation= new ProductManager();
+productCreation.addProduct ("Aros", "Aros luna plata 925", 5200, "https://joyaspremium.cl/wp-content/uploads/2021/12/DAD-25.png", "abc123", 25);
+productCreation.addProduct ("Anillo", "Anillo de plata 925", 8500, "https://e7.pngegg.com/pngimages/798/638/png-clipart-wedding-ring-jewellery-silver-gold-ring-love-gemstone.png", "xyz456", 15);
+console.log("Productos: ", productCreation.getProduct());
 
 
 // Ejemplo de prueba de busqueda por ID
-nuevoProducto.getProductById (4); 
+productCreation.getProductById (4);
+productCreation.getProductById (2);
 
 // Ejemplo prueba: ingreso de producto incompleto
-nuevoProducto.addProduct ("Pulsera"); 
+productCreation.addProduct ("Pulsera"); 
 
 //Ejemplo de prueba al ingresar un codigo repetido
-nuevoProducto.addProduct ("Collar", "Collar Flor de loto", 12500, "https://ripleycl.imgix.net/https%3A%2F%2Ftodojoyas.cl%2F3587-large_default%2Fcollar-plata-925-flor-de-loto.jpg?w=750&h=555&ch=Width&auto=format&cs=strip&bg=FFFFFF&q=60&trimcolor=FFFFFF&trim=color&fit=fillmax&ixlib=js-1.1.0&s=bf4c944ffcff14348ccb37fbc09f4d18", "xyz456", 15);
+productCreation.addProduct ("Collar", "Collar Flor de loto", 12500, "https://ripleycl.imgix.net/https%3A%2F%2Ftodojoyas.cl%2F3587-large_default%2Fcollar-plata-925-flor-de-loto.jpg?w=750&h=555&ch=Width&auto=format&cs=strip&bg=FFFFFF&q=60&trimcolor=FFFFFF&trim=color&fit=fillmax&ixlib=js-1.1.0&s=bf4c944ffcff14348ccb37fbc09f4d18", "xyz456", 15);
 
 
 
